7e4111d292c4dbb5f7de464cf4dfa6097a83c976
davien21 <chidiebereekennia@gmail.com>
2020-04-04 04:18:29 +0100

    Add Click event handler for simulate btn

--- assets/js/jquery_functions.js
+++ assets/js/jquery_functions.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+	$('#sim-btn').click(function(){
+  		$('#what-to-do').addClass('d-none')
+  		$('#input-section').removeClass('d-none')
+	});
+	$('#track-btn').click(function(){
+		
+	})
+	$('#sim-parameters').submit(function(e){
+		e.preventDefault();
+		let all_inputs = $('#sim-parameters').serialize();
+	 	$.post("./backends/backend.php",{all_inputs}, function(data, status){
+	    	alert("Data: " + data + "\nStatus: " + status);
+  		});
+  		$('#table-section').removeClass('d-none')
+  		$('#input-section').addClass('d-none')
+	})
 	
 }); 
