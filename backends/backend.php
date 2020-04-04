1f9f37fd80b4e62db1a9f5d4cdbfe1c495085052
davien21 <chidiebereekennia@gmail.com>
2020-04-04 07:11:51 +0100

    Add Compound Investment function

--- backends/backend.php
+++ backends/backend.php
@@ -1,22 +1,29 @@
 <?php
+	require 'form-validations.php';
 	$col_head_1 = "S/N";
 	$col_head_2 = "Investment";
 	$col_head_3 = "Yield";
 	$col_head_4 = "C./Investment";
 	$col_head_5 = "C./Yield";
 	$col_head_6 = "C./Profit";
-	if ($_SERVER["REQUEST_METHOD"] === 'POST') {
-		print_r($_POST);
-		if (isset($_POST['request'])) {
-			$request = sanitizeInputs($_POST['request']);
-			if ($request === 'delete_jobs') {
-				// print_r($_POST);
-				$id = sanitizeInputs($_POST['id']);
-				$db_deleteOps = new DeleteQuery($database_name);
-				$db_deleteOps->deleteJob($id);
-			}
-		}
+	function return_yield($investment)
+	{
+		return $yield = $investment * 1.5;
 	}
-	
+	$amount = 30000;
+	// echo returnYield($amount);
+	function get_c_invest($initial_capital,$increment,$number) {
+		$array_of_c_investment = [];
+		$current_value = 0;
+		for ($i=0;$i<$number;$i++) {
+			$current_value += $initial_capital + $increment*$i;
+			array_push($array_of_c_investment, $current_value);
+		}
+		return $array_of_c_investment;
+	};
+	print_r(get_c_invest(20000,10000,10));
+	// function get_c_investment($type,'standard_c_investment') {
+
+	// }
 
 ?>
