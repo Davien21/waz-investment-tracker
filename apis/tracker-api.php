b8fe13f698a09413833a4324f9f78519ef8330da
davien21 <chidiebereekennia@gmail.com>
2020-04-05 02:34:12 +0100

    Add a function to determine and store all simulation data

--- apis/tracker-api.php
+++ apis/tracker-api.php
@@ -4,14 +4,43 @@
 	{
 		return $yield = $investment * 1.5;
 	}
+	$investment_simulation_data = function ($number,$first_investment,$increment) {
+		$data_array = [];
+		$compounded_investment = $compounded_yield = $compounded_profit = 0;
+		for ($i=0;$i<$number;++$i) {
+			$investment = $first_investment + ($increment*$i);
+			if ($i===0) {
+				$yield = 0;
+				$compounded_yield = 0;
+			}else {
+				$yield = $investment * 1.5;
+				$compounded_yield += $yield;
+			}
+			$profit = $yield - $investment;
+			$compounded_investment += $investment;
+			$compounded_profit += $profit;
+			$block_data = 
+				[
+					'index'=>$i+1,'investment'=>$investment,
+					'yield'=>$yield,'profit'=>$profit,
+					'compounded_investment'=>$compounded_investment,
+					'compounded_yield'=>$compounded_yield,
+					'compounded_profit'=>$compounded_profit
+				];
+			array_push($data_array, $block_data);
+		}
+		return $data_array;
+	};
 	if ($_SERVER["REQUEST_METHOD"] === 'POST') {
-		print_r($_POST);
 		if (isset($_POST['request'])) {
 			$request = sanitizeInputs($_POST['request']);
-			if ($request === 'simulation') {
-				$capital = sanitizeInputs($_POST['capital']);
+			if ($request === 'simulate') {
+				$investment = sanitizeInputs($_POST['investment']);
 				$number = sanitizeInputs($_POST['number']);
 				$increment = sanitizeInputs($_POST['increment']);
+				$capital = sanitizeInputs($_POST['capital']);
+			
+				print_r($investment_simulation_data($number,$investment,$increment));
 
 			}
 		}
