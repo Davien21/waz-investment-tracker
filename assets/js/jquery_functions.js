9103d68f58321f96c8841903901bb540be4db2bf
davien21 <chidiebereekennia@gmail.com>
2020-04-06 06:19:47 +0100

    Implemented basic go back function to return to previous state of the page

--- assets/js/jquery_functions.js
+++ assets/js/jquery_functions.js
@@ -1,33 +1,64 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	/* ===== Simulator section ====== */
-
-	$('#sim-btn').click(function(){
+	function goBack() {
+		if ($('main').data('state')==='manner-of-input') {
+			$('#manner-of-input').addClass('d-none');
+  			$('#what-to-do').removeClass('d-none');
+			$('main').data('state','what-to-do');
+		}else if ($('main').data('state')==='manual-sim-table'){
+			$('#table-section .add-btn').addClass('d-none');
+			$('#table-section').addClass('d-none');
+			$('#manner-of-input').removeClass('d-none');
+			$('main').data('state','manner-of-input');
+		}else if ($('main').data('state')==='auto-sim-input') {
+			$('#manner-of-input').removeClass('d-none');
+  			$('section#auto-simulation').addClass('d-none');
+  			$('main').data('state','manner-of-input');
+		}else if ($('main').data('state')==='auto-sim-table') {
+			$('#table-section').addClass('d-none');
+	  		$('section#auto-simulation').removeClass('d-none');
+	  		$('#table-section tbody').empty();
+  			$('main').data('state','auto-sim-input');
+		}
+	}
+	$('#sim-btn').click(function() {
   		$('#what-to-do').addClass('d-none');
   		$('#manner-of-input').removeClass('d-none');
-  		// $('section#auto-simulation').removeClass('d-none');
-	});
-	$('#manner-of-input .back-btn').click(function(){
+  		$('main').data('state','manner-of-input');
+  	});
+	$('#manual-sim-btn').click(function() {
   		$('#manner-of-input').addClass('d-none');
-  		$('#what-to-do').removeClass('d-none');
+  		$('#table-section .add-btn').removeClass('d-none');
+  		$('#table-section').removeClass('d-none');
+  		$('main').data('state','manual-sim-table');
 	});
-	$('#auto-sim-btn').click(function(){
+	$('#auto-sim-btn').click(function() {
   		$('#manner-of-input').addClass('d-none');
   		$('section#auto-simulation').removeClass('d-none');
+  		$('main').data('state','auto-sim-input');
 	});
-	$('section#auto-simulation .back-btn').click(function(){
-  		$('#manner-of-input').removeClass('d-none');
+	$('button#submit-sim-values').click(function(e) {
+		e.preventDefault();
+		if ($('main').data('state','auto-sim-table'));
+		let all_inputs = $('#sim-parameters').serialize()+'&request=simulate';
   		$('section#auto-simulation').addClass('d-none');
+		$('#table-section').removeClass('d-none');
+  		$('main').data('state','auto-sim-table');
+	 	$.post("./apis/tracker-api.php",all_inputs, function(data, status) {
+	    	let investment_data = JSON.parse(data);	
+	    	for (let i of investment_data) {
+	    		$('#table-section tbody').append(table_row(i));
+	    	}
+  		});
 	})
-	$('#table-section .back-btn').click(function(){
-		$('#what-to-do').addClass('d-none');
-		$('#table-section').addClass('d-none');
-  		$('section#auto-simulation').removeClass('d-none');
-  		$('#table-section tbody').empty();
-	})
+	$('.back-btn').click(function() {
+  		goBack();
+	});
 	let table_row =  (data_object) => {
 		// if (data_object[i]).includes()
 		let table_row = 
-			`<tr>
+			`
+			 <tr>
 				<td>${data_object.index}</td>
 				<td>&#8358;${data_object.investment}</td>
 				<td>&#8358;${data_object.yield}</td>
@@ -35,23 +66,11 @@ $(document).ready(function(){
 				<td>&#8358;${data_object.compounded_investment}</td>
 				<td>&#8358;${data_object.compounded_yield}</td>
 				<td>&#8358;${data_object.compounded_profit}</td>
-			 </tr>`;
+			 </tr>
+			`;
 		return table_row;
 	}
-	$('#sim-parameters').submit(function(e){
-		e.preventDefault();
-		let all_inputs = $('#sim-parameters').serialize()+'&request=simulate';
-		$('#table-section').removeClass('d-none');
-	 	$.post("./apis/tracker-api.php",all_inputs, function(data, status) {
-	    	let investment_data = JSON.parse(data);	
-	    	for (let i of investment_data) {
-	    		$('#table-section tbody').append(table_row(i));
-	    	}
-  		});
-  		$('section#auto-simulation').addClass('d-none');
-	})
 	/* ===== Tracking section ====== */
-	$('#track-btn').click(function(){
-
+	$('#track-btn').click(function() {
 	})
 }); 
